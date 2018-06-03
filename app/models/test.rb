55a1d6b70c75e55ec0ef3ebfa765ae96063e6354
lestgabo <lestgabo@gmail.com>
2018-06-03 09:26:09 +0000

    resize uploaded images

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,16 @@
 class Test < ApplicationRecord
   has_many_attached :uploads # one-to-many relationship
   validates :name, presence: true
-  
   # this will allow all the image types
   validates :uploads, file_content_type: { allow: /^image\/.*/ }
   # if there is attachment
   validate :is_there_attached
+  
+  def thumbnail
+    require "mini_magick"
+    image = MiniMagick::Image.open(url_for(uploads))
+    image.resize "200x200"
+  end
   
   private 
   def is_there_attached
