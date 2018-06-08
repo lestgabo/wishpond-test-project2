f65f9276ce7bbe29c06ff43626f4f0efcb00e06e
lestgabo <lestgabo@gmail.com>
2018-06-08 11:19:43 +0000

    cors problem with production

--- config/application.rb
+++ config/application.rb
@@ -18,3 +18,8 @@ module AWishpondTestProject
     # the framework and any gems in your application.
   end
 end
+
+config.action_dispatch.default_headers = {
+  'Access-Control-Allow-Origin' => '*',
+  'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+}
